b1ad1dcb42e0ed2798fbd6f5b5e61cd57443bc4c
sundy8336 <sundy8336@fxiaoke.com>
2020-03-29 17:34:43 +0800

    feat

---
+++ JDBC/src/main/java/com/sdy/Class/Apple.java
@@ -0,0 +1,11 @@
+package com.sdy.Class;
+
+/**
+ * @author: sundy
+ * @date: 2020/3/29 17:32
+ * @description: 苹果类
+ */
+@Data
+public class Apple {
+    private
+}
---
+++ JDBC/src/main/java/com/sdy/lambda/AppleComparator.java
@@ -0,0 +1,15 @@
+package com.sdy.lambda;
+
+import java.util.Comparator;
+
+/**
+ * @author: sundy
+ * @date: 2020/3/29 17:31
+ * @description: 根据重量实现苹果的排序比较器
+ */
+public class AppleComparator implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+
+    }
+}
