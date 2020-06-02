056d3e48393d1353f6071e9656bf50de173a0b15
sundy <sundy_u@163.com>
2020-06-02 10:25:19 +0800

    feat: 单元测试

---
+++ src/main/java/com/sdy/scopk/Sum.java
@@ -0,0 +1,15 @@
+package com.sdy.scopk;
+
+/**
+ * @author: Sun Deyu
+ * @date: 2020/6/1 16:43
+ * @description:
+ */
+public class Sum {
+    public int exector(int first, int second) {
+//        if (first == -1){
+//            return 0;
+//        }
+        return first + second;
+    }
+}
--- src/main/java/com/sdy/thread/implement/ThreadMain.java
+++ src/main/java/com/sdy/thread/implement/ThreadMain.java
@@ -57,13 +57,11 @@ public class ThreadMain {
      * 和数据库池差不多
      * 避免了线程的创建和销毁造成的额外开销
      * java.util.concurrent
-     * <p>
      * Executor 负责线程的使用和调度的根接口
-     * |-- ExecutorService 线程池的主要接口
-     * |-- ThreadPoolExecutor 线程池的实现类
-     * |-- ScheduledExecutorService 接口，负责线程的调度
-     * |-- ScheduledThreadPoolExecutor （extends ThreadPoolExecutor implements ScheduledExecutorService）
-     * <p>
+     *      |-- ExecutorService 线程池的主要接口
+     *          |-- ThreadPoolExecutor 线程池的实现类
+     *          |-- ScheduledExecutorService 接口，负责线程的调度
+     *              |-- ScheduledThreadPoolExecutor （extends ThreadPoolExecutor implements ScheduledExecutorService）
      * Executors 工具类
      * 提供了创建线程池的方法
      */
@@ -115,3 +113,37 @@ public class ThreadMain {
         }
     }
 }
+
+/**
+ *优点
+ *
+ * 重用线程池中的线程，避免因为线程的创建和销毁带来的性能开销。
+ * 有效控制线程的最大并发数，避免因大量的线程之间互相抢占系统资源而导致的阻塞 。
+ * 能够对线程进行简单的管理，并提供定时执行和执行循环间隔执行等功能
+ *
+ * public ThreadPoolExecutor(int corePoolSize,
+ *                           int maximumPoolSize,
+ *                           long keepAliveTime,
+ *                           TimeUnit unit,
+ *                           BlockingQueue<Runnable> workQueue,
+ *                           ThreadFactory threadFactory,
+ *                           RejectedExecutionHandler handler) {}
+ *
+ *变量
+ *
+ * corePoolSize: 核心线程数
+ * maximumPoolSize: 最大线程数
+ * workQueue:任务队列，提交的Runnable对象存储在这里。
+ * keepAliveTime: 非核心线程存活时间
+ * unit:keepAliveTime的时间单位。
+ * threadFactory:为线程池提供新线程的工厂。
+ * handler：异常处理策略。
+ *
+ *规则
+ *
+ * 如果此时线程池中的数量小于corePoolSize，即使线程池中的线程都处于空闲状态，也要创建新的线程来处理被添加的任务。
+ * 如果此时线程池中的数量等于 corePoolSize，但是缓冲队列 workQueue未满，那么任务被放入缓冲队列。
+ * 如果此时线程池中的数量大于corePoolSize，缓冲队列workQueue满，并且线程池中的数量小于maximumPoolSize，建新的线程来处理被添加的任务。
+ * 如果此时线程池中的数量大于corePoolSize，缓冲队列workQueue满，并且线程池中的数量等于maximumPoolSize，那么通过 handler所指定的策略来处理此任务。
+ *
+ */
