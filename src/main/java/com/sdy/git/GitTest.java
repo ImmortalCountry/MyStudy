6fff2ba01c35af6f82e89f93ceb500575f199267
sundy8336 <sundy8336@fxiaoke.com>
2020-06-03 22:38:17 +0800

    feat 测试分支合并

--- src/main/java/com/sdy/git/GitTest.java
+++ src/main/java/com/sdy/git/GitTest.java
@@ -7,8 +7,6 @@ package com.sdy.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("我是主分支");
-        System.out.println("测试分支合并");
-        System.out.println("我是测试分支");
+        System.out.println("我是 merge2");
     }
 }
