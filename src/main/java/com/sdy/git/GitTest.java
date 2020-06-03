32959507d309d521130ad3756b135d4cfff6e8bb
sundy8336 <sundy8336@fxiaoke.com>
2020-06-03 23:05:26 +0800

    kaifa

--- src/main/java/com/sdy/git/GitTest.java
+++ src/main/java/com/sdy/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("我是 test");
         System.out.println("test分支新增");
+        System.out.println("我开始开发");
     }
 }
