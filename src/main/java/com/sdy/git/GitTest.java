9958dd8a107e2a2694f7d138f5ad3df82a357fb1
sundy8336 <sundy8336@fxiaoke.com>
2020-06-03 23:27:13 +0800

    kaifa

--- src/main/java/com/sdy/git/GitTest.java
+++ src/main/java/com/sdy/git/GitTest.java
@@ -8,6 +8,7 @@ package com.sdy.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("重构");
+        System.out.println("bugfix1");
         System.out.println("重构开发");
         System.out.println("补充");
         System.out.println("又开发了一个新功能");
