3bb87431c696df1a9e438e594f5f49bd7dce79c4
sundy8336 <sundy8336@fxiaoke.com>
2020-06-03 22:22:22 +0800

    study git

---
+++ src/main/java/com/sdy/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.sdy.git;
+
+/**
+ * @author: sundy
+ * @date: 2020/6/3 22:21
+ * @description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("我是主分支");
+    }
+}
