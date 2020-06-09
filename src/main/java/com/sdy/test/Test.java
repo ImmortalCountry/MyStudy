563a42b756b807870e81784dd8b2c4a9b2b2b7b2
sundy <sundy_u@163.com>
2020-06-09 15:08:49 +0800

    feat: Test Main

---
+++ src/main/java/com/sdy/test/Test.java
@@ -0,0 +1,15 @@
+package com.sdy.test;
+
+/**
+ * @author: Sun Deyu
+ * @date: 2020/6/8 19:28
+ * @description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        int i = 1000;
+        for (int j = 0; j < i; j++) {
+            System.out.println(j);
+        }
+    }
+}
